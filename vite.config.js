7b7ca2baea5c4d2759683c41c9a63c4dee826ff2
mrifkiehasib <rifkiesama@gmail.com>
2025-09-24 09:23:36 +0700

    setup gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   // MENGGUNAKAN PATH RELATIF UNTUK MEMPERBAIKI MASALAH DEPLOY
-  base: './', 
+  base: '/pnglass/', 
 })
